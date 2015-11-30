1198c3d038c681224a126c498d89145b7a54ddec
Maddy <maddy531@gmail.com>
2015-11-30 11:41:20 -0500

    Added a files

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-script.js
+var app = angular.module('serviceModule', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider.when('/palindromeView',{
+    controller: 'palindromeCtrl',
+    templateUrl: "palindromeView.html"
+  });
+
+  $routeProvider.otherwise({redirectTo:"/palindromeView"})
+
+});
