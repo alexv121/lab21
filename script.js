33469beb75aebd8257d078ddbfbd977ab09f1652
Michael Larkins <mlarkinsii@gmail.com>
2015-12-01 08:06:34 -0500

    updated index, script, and view2

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-script.js
+var app = angular.module('serviceModule', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider.when(
+    '/palindromeView',{
+    controller: 'palindromeCtrl',
+    templateUrl: "palindromeView.html"
+  });
+  $routeProvider.when('/view2',{
+  controller: 'controllerView2',
+  templateUrl: "view2.html"
+});
+
+  $routeProvider.otherwise({redirectTo:"/palindromeView"})
+
+});
